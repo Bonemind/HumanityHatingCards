0aef4909364b7064ca6125084546a35db875bbb9
Bonemind <Subhime@gmail.com>
2014-11-24 01:27:50 +0100

    Decks are now downloaded only when they are not available

--- HHC/Assets/Scripts/DeckLoader.cs
+++ HHC/Assets/Scripts/DeckLoader.cs
@@ -7,13 +7,31 @@ using Newtonsoft.Json;
 
 public class DeckLoader : MonoBehaviour {
     /// <summary>
+    /// Path of the decks
+    /// </summary>
+    private static string DeckPath = Application.persistentDataPath + "/Decks";
+
+    /// <summary>
     /// Loads a deck from a given deck code
     /// </summary>
     /// <param name="deckCode">The cardcast deck code</param>
     /// <returns>The parsed deck</returns>
     public static Deck LoadDeck(string deckCode)
     {
-        string deckJson = LoadJsonFromApi(ConfigManager.GetValue("CardListUrl").Replace("<DECK_CODE>", deckCode));
+        if (!Directory.Exists(DeckPath))
+        {
+            Directory.CreateDirectory(DeckPath);
+        }
+        string currDeckPath = DeckPath + "/" + deckCode;
+        if (!Directory.Exists(currDeckPath))
+        {
+            Directory.CreateDirectory(currDeckPath);
+            string cardsJson = LoadJsonFromApi(ConfigManager.GetValue("CardListUrl").Replace("<DECK_CODE>", deckCode));
+            string deckInfoJson = LoadJsonFromApi(ConfigManager.GetValue("DeckInfoUrl").Replace("<DECK_CODE>", deckCode));
+            File.WriteAllText(currDeckPath + "/cards.json", cardsJson);
+            File.WriteAllText(currDeckPath + "/deck.json", deckInfoJson);
+        }
+        string deckJson = File.ReadAllText(currDeckPath + "/cards.json");
         Debug.Log(deckJson);
         Debug.Log("Loaded deck json");
         Deck deck = JsonConvert.DeserializeObject<Deck>(deckJson);
